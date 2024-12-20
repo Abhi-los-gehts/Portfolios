70635a1278b96034e9346ca2da0b64d4a08e91ca
Abhilasha <Abhilasha.reva61@gmail.com>
2024-12-20 11:50:46 +0100

    initial commit

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// String
+var a="hello Abhi"
+console.log(a)
+
+// addition
+var bum= 10 + 2
+console.log(bum)
+
+// division
+let div = 20 / 10
+console.log(div)
+
+const num1 =10
+let num2=2
+console.log(num1+num2)
